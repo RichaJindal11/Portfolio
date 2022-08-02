04335386b7830fae6fae5cf766c2df59ade59208
RichaJindal11 <richa0582.be20@chitkara.edu.in>
2022-08-02 17:54:04 +0530

    Portfolio

---
+++ FormPage/main.js
@@ -0,0 +1,21 @@
+document.getElementById("submit").addEventListener("click", (e)=>{
+    e.preventDefault()
+    const first = document.getElementById('fname').value
+    console.log(first)
+    const last = document.getElementById("lname").value
+    const email = document.getElementById("Email").value
+    const password = document.getElementById("password").value
+    // const Gender = document.getElementById("gender").value
+    // const Message = document.getElementById("message").value
+
+
+
+    const myObject = new Object()
+    myObject.first_name = first;
+    myObject.last_name = last;
+    myObject.Email = email;
+    myObject.Password = password;
+    // myObject.Message= 
+    console.log(myObject)
+
+})
